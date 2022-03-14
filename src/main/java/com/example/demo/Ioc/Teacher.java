63d0118324b234ccb99c4cef962280577695c7d7
wes0316 <wes0316@gmail.com>
2022-03-14 17:02:58 +0800

    0310

--- src/main/java/com/example/demo/Ioc/Teacher.java
+++ src/main/java/com/example/demo/Ioc/Teacher.java
@@ -1,11 +1,13 @@
 package com.example.demo.Ioc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Teacher {
-
+    @Autowired
+    @Qualifier("canonPrinter")//指定要HP還是Canon印表機實體，首字均為小寫
     private  Printer printer;
 
     public void teach()
